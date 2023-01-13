e3e2c724d23900ae635c787616db4b33a1a7be66
albrechetti <luan@albrechetti.com>
2023-01-13 20:28:09 -0300

    resize test 2

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "b61943407b1d43b1a914adc2ff3165a8",
-"index.html": "e5cb77c120277bd9f12e29c4e1d907bb",
-"/": "e5cb77c120277bd9f12e29c4e1d907bb",
-"main.dart.js": "6d996e012ad916d4f11adf7548771794",
+"index.html": "07f58920da0ac6d106aae83f8a0d96b9",
+"/": "07f58920da0ac6d106aae83f8a0d96b9",
+"main.dart.js": "9caed5d3e50862fc51eb5d92e280c7b0",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
