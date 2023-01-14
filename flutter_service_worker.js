58f7a554d7b28b3ff870e8583242b9946e7ee89c
albrechetti <luan@albrechetti.com>
2023-01-13 21:56:38 -0300

    test html render

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "b61943407b1d43b1a914adc2ff3165a8",
-"index.html": "0210a173948b90e2b28f1bea033d5cd0",
-"/": "0210a173948b90e2b28f1bea033d5cd0",
-"main.dart.js": "3b8fa6c9de42cd95f184628187710fe1",
+"index.html": "e738851e17a3881480aeb92f0c557529",
+"/": "e738851e17a3881480aeb92f0c557529",
+"main.dart.js": "5a240a9be90ef8ecd5db264c32d71291",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
